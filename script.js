cccd438daf7b402c89e187116513397456193b7e
Mackenzie Schutz <mshoots64@gmail.com>
2020-05-03 11:12:32 -0500

    added contents to one index with d-none classes

--- script.js
+++ script.js
@@ -1,43 +1,99 @@
-// Facebook and Linkedin Buttons
-var linkedinBtn = document.getElementById("linkedin");
-var fbBtn = document.getElementById("facebook");
+window.onload=function(){
 
-linkedinBtn.addEventListener('click',linkedinBtnF);
-fbBtn.addEventListener('click', fbBtnF);
+// nav bar buttons 
 
-function linkedinBtnF () {
-    window.open("https:/www.linkedin.com/in/mackenzie-schutz-2560a6199")
-}
+$("#about-btn").on("click", function () {
+    $("#container0").removeClass("d-none");
+    $("#container1").addClass("d-none");
+    $("#container2").addClass("d-none");
+})
 
-function fbBtnF () {
-    window.open("https://www.facebook.com/mackenzie.schutz")
-}
+$("#portfolio-btn").on("click", function () {
+    $("#container1").removeClass("d-none");
+    $("#container0").addClass("d-none");
+    $("#container2").addClass("d-none");
+})
+
+$("#contact-btn").on("click", function () {
+    $("#container2").removeClass("d-none");
+    $("#container0").addClass("d-none");
+    $("#container1").addClass("d-none");
+})
+
+// const aboutBtn = document.getElementById("about-btn");
+// const portfolioBtn = document.getElementById("portfolio-btn");
+// const contactBtn = document.getElementById("contact-btn");
+
+// function aboutF () {
+// document.getElementById("container0").classList.remove("d-none");
+// document.getElementById("container1").classList.add("d-none");
+// document.getElementById("container2").classList.add("d-none");
+// }
+
+// function portfolioF () {
+//     document.getElementById("container1").classList.remove("d-none");
+//     document.getElementById("container0").classList.add("d-none");
+//     document.getElementById("container2").classList.add("d-none");
+// }
+
+// function contactF () {
+//     document.getElementById("container2").classList.remove("d-none");
+//     document.getElementById("container1").classList.add("d-none");
+//     document.getElementById("container0").classList.add("d-none");
+// }
+
+// aboutBtn.addEventListener('click', aboutF);
+// portfolioBtn.addEventListener('click', portfolioF );
+// contactBtn.addEventListener('click', contactF);
+
 
 // Portfolio Buttons
-const weatherBtn = document.getElementById("weather-btn");
-const passBtn = document.getElementById("password-btn");
-const cocktailBtn = document.getElementById("cocktail-btn");
-const quizBtn = document.getElementById("quiz-btn");
+// const weatherBtn = document.getElementById("weather-btn");
+// const passBtn = document.getElementById("password-btn");
+// const cocktailBtn = document.getElementById("cocktail-btn");
+// const quizBtn = document.getElementById("quiz-btn");
 
-weatherBtn.addEventListener('click', weatherF);
-passBtn.addEventListener('click', passF);
-cocktailBtn.addEventListener('click', drinkingF);
-quizBtn.addEventListener('click', quizF);
+// function weatherF (e) {
+//     e.preventDefault()
+//     window.open("https://knzmck.github.io/weather-dashboard/")
+// }
 
-
-function weatherF () {
-    window.open("https://knzmck.github.io/weather-dashboard/")
-}
-
-function passF () {
-    window.open("https://knzmck.github.io/password-generator/")
-}
+// function passF (e) {
+//     e.preventDefault()
+//     window.open("https://knzmck.github.io/password-generator/")
+// }
 
 
-function drinkingF () {
-    window.open("https://knzmck.github.io/Drinking-At-Home/")
-}
+// function drinkingF (e) {
+//     e.preventDefault()
+//     window.open("https://knzmck.github.io/Drinking-At-Home/")
+// }
 
-function quizF () {
-    window.open("https://knzmck.github.io/code-quiz/")
+// function quizF (e) {
+//     e.preventDefault()
+//     window.open("https://knzmck.github.io/code-quiz/")
+// }
+
+// weatherBtn.addEventListener('click', weatherF);
+// passBtn.addEventListener('click', passF);
+// cocktailBtn.addEventListener('click', drinkingF);
+// quizBtn.addEventListener('click', quizF);
+
+// // Facebook and Linkedin Buttons
+// var linkedinBtn = document.getElementById("linkedin");
+// var fbBtn = document.getElementById("facebook");
+
+// function linkedinBtnF (e) {
+//     e.preventDefault()
+//     window.open("https:/www.linkedin.com/in/mackenzie-schutz-2560a6199")
+// }
+
+// function fbBtnF (e) {
+//     e.preventDefault()
+//     window.open("https://www.facebook.com/mackenzie.schutz")
+// }
+
+// linkedinBtn.addEventListener('click',linkedinBtnF);
+// fbBtn.addEventListener('click', fbBtnF);
+
 }
